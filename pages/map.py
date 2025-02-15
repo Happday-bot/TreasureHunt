e323681f4b7c84f57fc6c3277ac7b6d73888d61e
Alfred Sam <alfredsam2006@gmail.com>
2025-02-15 05:34:26 +0530

    Update map.py

--- pages/map.py
+++ pages/map.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Map")
 
 # Display the image
-st.image('./pages/map.png', caption="I always hang in your key", use_container_width =True)
+st.image('./pages/map.png', caption="TREASURE MAP", use_container_width =True)
